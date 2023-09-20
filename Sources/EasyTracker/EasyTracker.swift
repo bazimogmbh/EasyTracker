2d6b41323e2df977b38e1a4c97a5475b27c544fa
Korsun <jenya.korsun@gmail.com>
2023-09-20 10:56:48 +0300

    add Foundation

--- Sources/EasyTracker/EasyTracker.swift
+++ Sources/EasyTracker/EasyTracker.swift
@@ -1,72 +1,55 @@
-//public struct EasyTracker {
-//    public private(set) var text = "Hello, World!"
-//
-//    public init() {
-//    }
-//}
-//
-//
-////
-////
-//import Foundation
-//
-public enum EasyTracker {
-    public static func configure() {
-        
-    }
-}
-
+import Foundation
 //import AppTrackingTransparency
 //import AdSupport
 //import Network
 //import SwiftyStoreKit
 
-//protocol TrackServiceProtocol {
-//    static func configure()
-//    static func trackPurchase(detail: String)
-//}
-//
-//enum DefaultsKey: String {
-//    case userId
-//}
-//
-//public enum TrackService: TrackServiceProtocol {
-//    enum TrackingKey: String {
-//        case userId, idfa, vendorID, appName, appVersion, appBuild, appLocale, country, iosVersion, device, bundleId, appId
-//        case localizedPrice, price, currency, productId, purchaseToken, orderId
-//    }
-//
-//    private static var userId: String = ""
-//    private static var idfa: String = ""
-//    private static var vendorID: String = ""
-//
-////    static func addObserver() {
-////           observerToken = NotificationCenter.default.addObserver(
-////               forName: UIApplication.didBecomeActiveNotification,
-////               object: nil,
-////               queue: nil
-////           ) { _ in
-////               // Handle the didBecomeActiveNotification here
-////               // You can perform actions here when the app becomes active
-////           }
-////       }
-//
-//
-//    static func configure() {
-//        setupUserId()
-//        sendData()
-//
-////        let observerToken = NotificationCenter.default.addObserver(
-////            forName: UIApplication.didBecomeActiveNotification,
-////            object: nil,
-////            queue: nil
-////        ) { _ in
-////            ATTrackingManager.requestTrackingAuthorization { status in
-////                sendData()
-////            }
-////        }
-//
-//        func sendData() {
+protocol TrackServiceProtocol {
+    static func configure()
+    static func trackPurchase(detail: String)
+}
+
+enum DefaultsKey: String {
+    case userId
+}
+
+public enum EasyTracker: TrackServiceProtocol {
+    enum TrackingKey: String {
+        case userId, idfa, vendorID, appName, appVersion, appBuild, appLocale, country, iosVersion, device, bundleId, appId
+        case localizedPrice, price, currency, productId, purchaseToken, orderId
+    }
+
+    private static var userId: String = ""
+    private static var idfa: String = ""
+    private static var vendorID: String = ""
+
+//    static func addObserver() {
+//           observerToken = NotificationCenter.default.addObserver(
+//               forName: UIApplication.didBecomeActiveNotification,
+//               object: nil,
+//               queue: nil
+//           ) { _ in
+//               // Handle the didBecomeActiveNotification here
+//               // You can perform actions here when the app becomes active
+//           }
+//       }
+
+
+    static func configure() {
+        setupUserId()
+        sendData()
+
+//        let observerToken = NotificationCenter.default.addObserver(
+//            forName: UIApplication.didBecomeActiveNotification,
+//            object: nil,
+//            queue: nil
+//        ) { _ in
+//            ATTrackingManager.requestTrackingAuthorization { status in
+//                sendData()
+//            }
+//        }
+
+        func sendData() {
 //            let idfa = ASIdentifierManager.shared().advertisingIdentifier.uuidString
 //            self.idfa = idfa
 //
@@ -103,44 +86,44 @@ public enum EasyTracker {
 //            ]
 //
 //            print("ANALITIC: \(data)")
-//        }
-//    }
-//
-//    static func trackPurchase(detail: String) {
-//        //       let receipt = detail.transaction.transactionReceipt
-//        // email
-//        // encription needed
-//
-//        //        let data: [String: String] = [
-//        //            TrackingKey.userId.rawValue: self.userId,
-//        //            TrackingKey.productId.rawValue: detail.productId,
-//        //            TrackingKey.localizedPrice.rawValue: detail.product.localizedPrice ?? "",
-//        //            TrackingKey.price.rawValue: detail.product.price.stringValue,
-//        //            TrackingKey.currency.rawValue: detail.product.priceLocale.currencyCode ?? "",
-//        //            TrackingKey.purchaseToken.rawValue: "",
-//        //            TrackingKey.orderId.rawValue: "",
-//        //        ]
-//        //
-//        //        print("ANALITIC: \(data)")
-//    }
-//
-//    static private func setupUserId() {
-//        if let userId: String = getFromDefaults(.userId) {
-//            self.userId = userId
-//        } else {
-//            self.userId = "\(UUID().uuidString.replacingOccurrences(of: "-", with: "").lowercased())"
-//            self.saveInDefaults(self.userId, by: .userId)
-//        }
-//    }
-//
-//    static private func saveInDefaults(_ value: Any?, by key: DefaultsKey) {
-//        UserDefaults.standard.set(value, forKey: key.rawValue)
-//    }
-//
-//    static private func getFromDefaults<T>(_ key: DefaultsKey) -> T? {
-//        return UserDefaults.standard.value(forKey: key.rawValue) as? T
-//    }
-//}
+        }
+    }
+
+    static func trackPurchase(detail: String) {
+        //       let receipt = detail.transaction.transactionReceipt
+        // email
+        // encription needed
+
+        //        let data: [String: String] = [
+        //            TrackingKey.userId.rawValue: self.userId,
+        //            TrackingKey.productId.rawValue: detail.productId,
+        //            TrackingKey.localizedPrice.rawValue: detail.product.localizedPrice ?? "",
+        //            TrackingKey.price.rawValue: detail.product.price.stringValue,
+        //            TrackingKey.currency.rawValue: detail.product.priceLocale.currencyCode ?? "",
+        //            TrackingKey.purchaseToken.rawValue: "",
+        //            TrackingKey.orderId.rawValue: "",
+        //        ]
+        //
+        //        print("ANALITIC: \(data)")
+    }
+
+    static private func setupUserId() {
+        if let userId: String = getFromDefaults(.userId) {
+            self.userId = userId
+        } else {
+            self.userId = "\(UUID().uuidString.replacingOccurrences(of: "-", with: "").lowercased())"
+            self.saveInDefaults(self.userId, by: .userId)
+        }
+    }
+
+    static private func saveInDefaults(_ value: Any?, by key: DefaultsKey) {
+        UserDefaults.standard.set(value, forKey: key.rawValue)
+    }
+
+    static private func getFromDefaults<T>(_ key: DefaultsKey) -> T? {
+        return UserDefaults.standard.value(forKey: key.rawValue) as? T
+    }
+}
 //
 //fileprivate extension UIDevice {
 //    var modelName: String {
