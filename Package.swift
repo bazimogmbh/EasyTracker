e651fd184087191cc760fb818c47e9193a2d5a9b
Korsun <jenya.korsun@gmail.com>
2023-09-20 10:45:46 +0300

    add "EasyTracker"

--- Package.swift
+++ Package.swift
@@ -20,6 +20,7 @@ let package = Package(
         .target(
             name: "EasyTracker",
             dependencies: [
+                "EasyTracker",
 //                .product(name: "Foundation", package: "swift-corelibs-foundation"), // UIKit depends
             ]),
         .testTarget(
